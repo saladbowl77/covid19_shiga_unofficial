ea211d2f122e97fd8133961053613ad726a2ff38
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2020-07-23 23:04:29 +0900

    7/23 data v1.4.6

--- js/daily_records_patients_all.js
+++ js/daily_records_patients_all.js
@@ -43,8 +43,8 @@ function patients_all_Chart(data) {
             ticks: {
              fontColor: "rgb(0, 0, 0, 0)",
              min: 0,
-             max: 150,
-             stepSize: 30
+             max: 160,
+             stepSize: 40
              }
           }]
         }
